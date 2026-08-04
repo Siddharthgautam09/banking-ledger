698f7e4408afb45abfcafaa3e72210d2053f45fc
Siddharthgautam09 <siddharth.mtc09@gmail.com>
2026-08-04 10:25:59 +0530

    Implement account balance retrieval and associated user accounts endpoint

--- src/controller/account.controller.js
+++ src/controller/account.controller.js
@@ -5,7 +5,7 @@ import Account from '../model/account.model.js'
  * - Create account for a authorized user
  */
 
-export default async function createAccountController ( req, res){
+export  async function createAccountController ( req, res){
 
     const user = req.user;
 
@@ -18,4 +18,57 @@ export default async function createAccountController ( req, res){
         account
     })
 
+}
+
+
+/**
+ * -GET balance of an account associated to the user
+ */
+
+export async function getAccountBalanceController(req, res) {
+
+    const {accountId} =  req.params;
+
+    const account = await Account.findOne({
+
+        _id: accountId,
+        user: req.user._id
+
+    })
+
+    if(!account){
+        res.status(404).json({
+            message:"Account Not Found"
+        })
+    }
+
+    const balance = await account.getBalance();
+
+    res.status(200).json({
+        message:"Balance Successfully Fetched",
+        account:account._id,
+        balance,
+
+    })
+
+}
+
+/**
+ *  -GET ACCOUNTS ASSOCIATED WITH A USER
+ */
+
+export async function getaccountController(req,res){
+
+    const user= req.user;
+
+    const accounts = await Account.find({
+        user: user._id
+    })
+
+    return res.status(200).json({
+        message:"Account Fetched Sucessfully",
+        user:user._id,
+        accounts
+
+    })
 }
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -2,7 +2,7 @@
 import express from 'express';
 
 import { authMiddleware } from '../middleware/auth.middleware.js'
-import createAccountController from '../controller/account.controller.js';
+import {createAccountController, getAccountBalanceController, getaccountController} from '../controller/account.controller.js';
 
 const accountRouter = express.Router();
 
@@ -13,6 +13,16 @@ const accountRouter = express.Router();
  * 
  */
 accountRouter.post('/',authMiddleware, createAccountController)
+
+/**
+ *   -GET /api/accounts/balance/:accountId
+ *  - Get the balance of an account by accountId
+ *   - Protected route
+ * 
+ */
+accountRouter.get('/balance/:accountId', authMiddleware, getAccountBalanceController)
+
+accountRouter.get('/',authMiddleware,getaccountController)
 
 
 export default accountRouter;
